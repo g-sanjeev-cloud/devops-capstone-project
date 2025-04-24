28144641173e40582684dcf73cbcd5b5f45a5860
SanjeevKumarG <sanjeev.gajam@encora.com>
2025-04-24 06:12:52 -0400

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -57,11 +57,10 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
-# ... place you code here to LIST accounts ...
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -74,15 +73,11 @@ def list_accounts():
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
-# ... place you code here to READ an account ...
-    ######################################################################
-    # READ AN ACCOUNT
-    ######################################################################
-    @app.route("/accounts/<int:account_id>", methods=["GET"])
+@app.route("/accounts/<int:account_id>", methods=["GET"])
     def get_accounts(account_id):
         """
         Reads an Account
@@ -95,16 +90,12 @@ def list_accounts():
             abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
         return account.serialize(), status.HTTP_200_OK
-        
+
+
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
-# ... place you code here to UPDATE an account ...
-    ######################################################################
-    # UPDATE AN EXISTING ACCOUNT
-    ######################################################################
-    @app.route("/accounts/<int:account_id>", methods=["PUT"])
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
     def update_accounts(account_id):
         """
         Update an Account
@@ -120,18 +111,12 @@ def list_accounts():
         account.update()
 
         return account.serialize(), status.HTTP_200_OK
-    
 
 
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-
-# ... place you code here to DELETE an account ...
-    ######################################################################
-    # DELETE AN ACCOUNT
-    ######################################################################
-    @app.route("/accounts/<int:account_id>", methods=["DELETE"])
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
     def delete_accounts(account_id):
         """
         Delete an Account
@@ -145,11 +130,10 @@ def list_accounts():
 
         return "", status.HTTP_204_NO_CONTENT
 
+
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
